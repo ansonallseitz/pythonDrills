3d9197b26b8c53e97f4fd658a547e84f77942873
ansonallseitz <ansonallseitz@gmail.com>
2022-08-16 23:01:24 -0400

    checkers

--- ch9/9.5_checkerboard.py
+++ ch9/9.5_checkerboard.py
@@ -12,6 +12,10 @@ class CheckerboardDemo:
 
         self.canvas = Canvas(window, width = 800, height = 750, bg = "#D3D3D3")
 
+        for y in range(10):
+            for x in range(10):
+                self.canvas.create_rectangle(x * 20, y * 20, (x + 1) * 20, (y + 1) * 20, fill='purple')
+
         window.mainloop()
 
 CheckerboardDemo()
