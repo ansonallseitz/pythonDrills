65e1b61170f7208f72a2f57af363bf9a64b0f0fd
Anson Allseitz <ansonallseitz@gmail.com>
2023-01-07 22:36:41 -0500

    more edits

--- Liang/ch14/14.4.py
+++ Liang/ch14/14.4.py
@@ -13,30 +13,33 @@ class LetterCounter():
 
 
         window.geometry("475x350")
-       # self.canvas = Canvas(window, width=200, height=200, bg="blue").grid(column=0, row=0)
-        #self.canvas.pack()
-        
+ 
+        #create the canvas 
         self.canvas = Canvas(window, width=200, height=200, bg="blue")
         self.canvas.grid(column=0, row=0)
 
         BtshowResults = Button(window, text ="Show Results", command = self.showResults).grid(column=9, row = 7)
         BtnBrowse = Button(window, text="Browse", command =self.browseFile).grid(column=7, row =7)
-        #BtshowResults.pack()
+        #create the buttons 
         self.EnterdFile = StringVar()
-        fileEntry = Entry(window, textvariable = self.EnterdFile).grid(column=4, row=7)
+        Entry(window, textvariable = self.EnterdFile).grid(column=4, row=7)
         Label(window, text="Enter a filename").grid(column=0, row=7)
+
+
 
         window.mainloop()
 
-    def showResults(self):
-        self.canvas.create_rectangle(75, 70, 150, 150, fill="red")
-   # def browse(self):
-      #  self.canvas.create_oval(75, 40, 112, 90, fill="purple")
-    
+
+    #brows file function 
     def browseFile(self):
         filenameForReading = askopenfilename()
-        
+        self.EnterdFile.set(filenameForReading)
+    
+    #show result function 
+    def showResults(self, filenameForReading):
+        letterCount = {} #create empty dictionary 
+        filenameForReading 
 
  
 
-LetterCounter()
+LetterCounter()     
